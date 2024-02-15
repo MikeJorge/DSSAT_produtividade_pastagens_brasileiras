3fae87de6d519e5b250132c2c9b2c7b585abaeea
pietrogf <pietro.gragfer@gmail.com>
2024-02-15 11:01:07 -0300

    Update 2_taxa_lotacao_critica.R
    Verificando pontos de morte na simulação

--- visualizacoes/2_taxa_lotacao_critica.R
+++ visualizacoes/2_taxa_lotacao_critica.R
@@ -15,3 +15,16 @@ p_load(data.table, lubridate, ggplot2, RSQLite, tidyr, dplyr,rgdal, sf, raster, 
 path <- "D:/arquivos/doutorado_michael/DSSAT-BPPP/outputs/outptus_michael.db" #endereco do banco de dados
 conn <- dbConnect(SQLite(), path)
 dbListTables(conn)
+
+# Verificando se houveram pontos onde o pasto morreu antes do fim da simulacao
+out <- dbGetQuery(conn,'select * from mensal_ext')
+
+last.year.per.point <- out %>%
+  group_by(PONTO_SIMULACAO) %>% 
+  summarise(ano = max(year))
+
+problems <- subset(last.year.per.point, ano < 2016) #nao houve pontos de morte, nem no cenario mais restritivo (extensivo)
+
+print(paste("Em", nrow(problems), "pontos de simulação a pastagem morreu antes de 2016", sep = " "))
+
+
