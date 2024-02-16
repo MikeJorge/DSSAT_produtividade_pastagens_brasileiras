dc19b7e7c1116878a3cb456514cd3fc9adc10459
pietrogf <pietro.gragfer@gmail.com>
2024-02-16 14:44:47 -0300

    Update 3_graficos_mapas_DSSAT.R

--- visualizacoes/3_graficos_mapas_DSSAT.R
+++ visualizacoes/3_graficos_mapas_DSSAT.R
@@ -15,3 +15,20 @@ p_load(data.table, lubridate, ggplot2, RSQLite, tidyr, dplyr,rgdal, sf, raster, 
 path <- "D:/arquivos/doutorado_michael/DSSAT-BPPP/outputs/outptus_michael.db" #endereco do banco de dados
 conn <- dbConnect(SQLite(), path)
 dbListTables(conn)
+
+# Mapas com o grid de biomassa seca (mensal e anual)----
+# Ingerindo os dados
+grid <- st_read("D:/arquivos/GPP/pastagens_IABS/analises_finais/BR_grid.shp")
+pontosfiltro <- fread("D:/arquivos/GPP/pastagens_IABS/analises_finais/pontos_filtro_N400.csv")
+
+out_mensal_1 <- dbGetQuery(conn,'select * from mensal_1')
+out_mensal_2 <- dbGetQuery(conn,'select * from mensal_2')
+out_mensal_3 <- dbGetQuery(conn,'select * from mensal_3')
+out_mensal_pot <- dbGetQuery(conn,'select * from mensal_pot')
+out_mensal_ext <- dbGetQuery(conn,'select * from mensal_ext')
+
+
+# Agrupando por mês e por ano
+head(out_mensal_1)
+mensal_1 <- out_mensal_1 %>% group_by(month, PONTO_SIMULACAO)%>%summarise(biomassa_mensal_1 = mean(`SUM(GANHO_DIARIO_CWAD)`, na.rm = T))
+mensal_2<- out_mensal_2 %>% group_by(month, PONTO_SIMULACAO)%>%summarise(biomassa_mensal_2 = mean(`SUM(GANHO_DIARIO_CWAD)`, na.rm = T))
