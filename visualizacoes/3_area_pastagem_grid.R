04c0da39e80acb09aae650134001213851477010
pietrogf <pietro.gragfer@gmail.com>
2024-03-22 15:20:28 -0300

    Update 3_area_pastagem_grid.R

--- visualizacoes/3_area_pastagem_grid.R
+++ visualizacoes/3_area_pastagem_grid.R
@@ -1,15 +1,15 @@
-###################################################
-# Contagem de área de pastagem por célula do grid
+############################################################################
+# Área de pastagem e região administrativa predominante por célula do grid
 # Pietro Gragnolati Fernandes
 # 15/03/2024
-###################################################
+############################################################################
 
 # Limpando area de trabalho
 rm(list=ls())
 
 # Instalando pacotes 
 library(pacman)
-p_load(data.table, raster, dplyr)
+p_load(data.table, raster, dplyr, RSQLite)
 
 # Definindo o diretorio e lendo os dados
 setwd("D:/arquivos/doutorado_michael/rasters")
@@ -34,3 +34,29 @@ for (i in 1:bss$n) {
   dbWriteTable(conn, 'contagem_pixel_area_pastagem', db, row.names = F, append = T)
   print(i)
 }
+
+# Sumarizando os resultados
+base <- dbGetQuery(conn, "SELECT * FROM contagem_pixel_area_pastagem")
+
+uf_pred <- base %>% group_by(grid, uf)%>%
+                 arrange(desc(area))%>%
+                 mutate(uf_pred = first(uf))
+
+uf_pred <- uf_pred%>%group_by(grid)%>%mutate(uf_pred = first(uf_pred))
+
+base <- base %>% group_by(grid)%>%
+  summarise(area_past_2022 = sum(area[past==1]))%>%
+  ungroup()
+
+base <- merge(base, uf_pred, by = "grid")
+base$uf <- base$uf_pred
+
+# Definindo a região admninistrativa do grid com base na uf predominante
+regioes <- fread(paste(path, "regioes_administrativas.csv"))
+base <- merge(base, regioes, by = "uf")
+
+base <- base[,c("grid", "uf", "nm_regiao", "area_past_2022")]
+
+# Exportando a tabela
+write.table(base, paste(path, "area_pastagem_e_regiao_predominante_grid.csv"),
+            row.names = F, sep = ";")
